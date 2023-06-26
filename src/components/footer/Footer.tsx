b5b5d6f55d7f9341d721377209526dc8861a7b71
sasanaryan <sasan4292@gmail.com>
2023-06-26 04:29:24 +0330

    update footer component with Considering the best practice

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,91 +1,85 @@
-import {
-  Facebook,
-  Instagram,
-  YouTube,
-  Twitter,
-} from "@mui/icons-material";
-
-import {mobile ,tablet } from "../../theme"
+import type { FC } from "react";
+import { Facebook, Instagram, YouTube, Twitter } from "@mui/icons-material";
 import { Stack } from "@mui/system";
-import styled from '@emotion/styled'
+import styled from "@emotion/styled";
+import { mobile, tablet } from "theme";
+import { footerMenu } from "data";
 
-
-
- const Container = styled.div`
-display: flex;
-flex-direction: column;
-width: 100%;
-color: #fff;
-background-color: #111111;
-justify-content:  space-between;
-${mobile({ flexDirection: "column" })}
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  color: #fff;
+  background-color: #111111;
+  justify-content: space-between;
+  ${mobile({ flexDirection: "column" })}
 `;
 
 const SocialContainer = styled.div`
-margin: 30px 30px 15px 15px;
-display: flex;
+  margin: 30px 30px 15px 15px;
+  display: flex;
 `;
-const SocialIcon = styled.div`
-width: 30px;
-height: 30px;
-border-radius: 50%;
-color: #111111;
-background-color: #${(props) => props.color};
-display: flex;
-align-items: center;
-justify-content: center;
-margin-right: 20px;
-${tablet({ marginRight: "10px" })}
-`;
-const SubItems = styled.span`
-font-size: 12px;
-font-family: 'Nunito', sans-serif;
-font-weight: 200; 
-color: #7a7a7d;
-cursor: pointer;
-&:hover {
-  color: #fff;
-}
-`;
-const Items = styled.span`
-font-size: 13px;
-color:#fff;
-font-family: 'Oswald', sans-serif;
-font-weight: 600;
-cursor: pointer;
-${tablet({ fontSize: '11px '})}
-`
 
-const Footer = () => {
+const SocialIcon = styled.div`
+  width: 30px;
+  height: 30px;
+  border-radius: 50%;
+  color: #111111;
+  background-color: #${(props) => props.color};
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin-right: 20px;
+  ${tablet({ marginRight: "10px" })}
+`;
+
+const SubItems = styled.span`
+  font-size: 12px;
+  font-family: "Nunito", sans-serif;
+  font-weight: 200;
+  color: #7a7a7d;
+  cursor: pointer;
+  &:hover {
+    color: #fff;
+  }
+`;
+
+const Items = styled.span`
+  font-size: 13px;
+  color: #fff;
+  font-family: "Oswald", sans-serif;
+  font-weight: 600;
+  cursor: pointer;
+  ${tablet({ fontSize: "11px " })}
+`;
+
+const Footer: FC = () => {
   return (
     <Container>
-     <Stack direction={{ xs: 'column', sm: 'row' }} justifyContent="space-between">
-     <Stack direction={{ xs: 'column', sm: 'row' }} gap={{ xs: 2, md: 5, lg: 10 }}>
-          <Stack direction='column' gap={2} sx={{margin: '30px '}} >
-            <Items>GIFT CARDS</Items>
-            <Items>PROMOTIONS</Items>
-            <Items>FIND A STORE</Items>
-            <Items>SIGN UP FOR EMAIL</Items>
-            <Items>BECOME A MEMBER</Items>
-            <Items>NIKE JOURNAL</Items>
-            <Items>SEND US FEEDBACK</Items>
+      <Stack
+        direction={{ xs: "column", sm: "row" }}
+        justifyContent="space-between"
+      >
+        <Stack
+          direction={{ xs: "column", sm: "row" }}
+          gap={{ xs: 2, md: 5, lg: 10 }}
+        >
+          <Stack direction="column" gap={2} sx={{ margin: "30px " }}>
+            {footerMenu.mainMenu.map((item) => (
+              <Items>{item}</Items>
+            ))}
           </Stack>
-          <Stack direction='column' gap={2} sx={{margin: '30px '}}>
+          <Stack direction="column" gap={2} sx={{ margin: "30px " }}>
             <Items>GET HELP</Items>
-            <SubItems>Order Status</SubItems>
-            <SubItems>Shipping and Delivery</SubItems>
-            <SubItems>Returns</SubItems>
-            <SubItems>Payment Options</SubItems>
-            <SubItems>Gift Card Balance</SubItems>
-            <SubItems>Contact Us</SubItems>
+            {footerMenu.getHelp.map((item) => (
+              <SubItems>{item}</SubItems>
+            ))}
           </Stack>
-          <Stack direction='column' gap={2} sx={{margin: '30px '}}>
+          <Stack direction="column" gap={2} sx={{ margin: "30px " }}>
             <Items>ABOUT NIKE</Items>
-            <SubItems>News</SubItems>
-            <SubItems>Careers</SubItems>
-            <SubItems>Investors</SubItems>
-            <SubItems>Purpose</SubItems>
-            <SubItems>Sustainability</SubItems>
+            {footerMenu.adoutNike.map((item) => (
+              <SubItems>{item}</SubItems>
+            ))}
           </Stack>
         </Stack>
         <SocialContainer>
@@ -96,26 +90,32 @@ const Footer = () => {
             <Facebook />
           </SocialIcon>
           <SocialIcon color="7a7a7d">
-          <YouTube />
+            <YouTube />
           </SocialIcon>
           <SocialIcon color="7a7a7d">
             <Instagram />
           </SocialIcon>
         </SocialContainer>
-     </Stack>
-     <Stack direction={{ xs: 'column', sm: 'row' }} justifyContent="flex-end" sx={{padding: '15px'}}>
-        <Stack direction={{ xs: 'column', sm: 'row' }} gap={3}>
-            <SubItems>Guides</SubItems>
-            <SubItems>Terms of Sale</SubItems>
-            <SubItems>Terms of Use</SubItems>
-            <SubItems>Nike Privacy Policy</SubItems>
-            <SubItems>Your Privacy Choices</SubItems>
+      </Stack>
+      <Stack
+        direction={{ xs: "column", sm: "row" }}
+        justifyContent="flex-end"
+        sx={{ padding: "15px" }}
+      >
+        <Stack direction={{ xs: "column", sm: "row" }} gap={3}>
+          {footerMenu.subMenu.map((item) => (
+            <SubItems>{item}</SubItems>
+          ))}
         </Stack>
-     </Stack>
-     <Stack direction={{ xs: 'column', sm: 'row' }} justifyContent="space-between" sx={{margin:'10px'}}>
-            <SubItems>© 2023 Nike, Inc. All Rights Reserved</SubItems>
-            <SubItems>CA Supply Chains Act</SubItems>
-        </Stack>
+      </Stack>
+      <Stack
+        direction={{ xs: "column", sm: "row" }}
+        justifyContent="space-between"
+        sx={{ margin: "10px" }}
+      >
+        <SubItems>© 2023 Nike, Inc. All Rights Reserved</SubItems>
+        <SubItems>CA Supply Chains Act</SubItems>
+      </Stack>
     </Container>
   );
 };
