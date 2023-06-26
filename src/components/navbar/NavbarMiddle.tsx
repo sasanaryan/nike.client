877f31914615867631a5eac399aa967832421153
sasanaryan <sasan4292@gmail.com>
2023-06-26 04:40:09 +0330

    refactor: update navbar and add some product images

--- src/components/navbar/NavbarMiddle.tsx
+++ src/components/navbar/NavbarMiddle.tsx
@@ -1,65 +1,80 @@
-import { Stack } from "@mui/system"
-import styled from '@emotion/styled'
-import { icons } from "../../data/data"
-import { Badge, Typography } from "@mui/material"
-import { Search } from "@mui/icons-material"
-import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
-import MenuIcon from '@mui/icons-material/Menu';
+import { useState } from "react";
+import type { FC } from "react";
+import { Link } from "react-router-dom";
+import styled from "@emotion/styled";
+import { Stack } from "@mui/system";
+import { Badge, Typography } from "@mui/material";
+import SearchBar from "components/searchBar";
+import MenuIcon from "@mui/icons-material/Menu";
+import { icons } from "data";
 
 const Container = styled.div`
-padding: 0px 30px 0px 20px;
-display: flex;
-flex-direction: row;
-justify-content: space-between;
-align-items: center;
-height: 70px;
-background-color: #ffffff;
-`
- const Logo = styled.img`
-margin: 0px 5px 0px 5px;
-position :relative;
-top:0px;
-height: 20px;
-font-weight: bold;
-`
-const BadgeContent = styled.div`
-position: absolute;
-display: flex;
-align-items: center;
-top:2px;
-font-size: 10px;
-left: 10px;
-`
+  padding: 0px 30px 0px 30px;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  height: 70px;
+  background-color: #ffffff;
+`;
 
-const NavbarMiddle = ( ) => {
+const Logo = styled.img`
+  margin: 0px 5px 0px 5px;
+  position: relative;
+  top: -2px;
+  height: 20px;
+  font-weight: bold;
+`;
+const BadgeContent = styled.div`
+  position: absolute;
+  display: flex;
+  align-items: center;
+  top: 0px;
+  font-size: 10px;
+  left: 10px;
+`;
+const StyledLink = styled(Link)`
+  color: black;
+  text-decoration: none;
+  &:hover {
+    color: gray;
+  }
+`;
+
+const NavbarMiddle: FC = () => {
+  const [openSideMenu, setOpenSideMenu] = useState<boolean>(false);
+  const [openSearchBar, setOpenSearchBar] = useState<boolean>(false);
+
   return (
     <>
-    <Container >
-        <Logo src={icons.logo} />
-      <Stack direction="row" justifyContent="space-around" spacing={2}  display= {{ md: 'none', xs: 'none'}}>
-        <Typography>New & Featured</Typography>
-        <Typography>Men</Typography>
-        <Typography>Women</Typography>
-        <Typography>Kids</Typography>
-        <Typography>Accesories</Typography>
-        <Typography>Sale</Typography>
-      </Stack>
-      <Stack direction="row" justifyContent="space-around" spacing={4} >
-        <Search />
-        <FavoriteBorderIcon sx={{ display: { md: 'none', xs: 'none'}}} />
-        <Badge>
-          <BadgeContent>
-            <Typography fontSize="13px">
-              1
-            </Typography>
-          </BadgeContent>
-          <Logo src={icons.cart}  />
-        </Badge>
-        <MenuIcon sx={{ display: { xl: 'none'} , fontSize: '28px'}}/>
-      </Stack>
-    </Container>
-    </>
-  )
-}
+      <Container>
+        <StyledLink to="/">
+          <Logo src={icons.logo} />
+        </StyledLink>
+        <Stack direction="row" alignItems="center" spacing={4}>
+          <SearchBar
+            setOpenSearchBar={setOpenSearchBar}
+            openSearchBar={openSearchBar}
+          />
 
-export default NavbarMiddle
+          <Stack direction="row" alignItems="center" gap={2}>
+            <StyledLink to="/cart">
+              <Badge>
+                <BadgeContent>
+                  <Typography fontSize="13px">4</Typography>
+                </BadgeContent>
+                <Logo src={icons.cart} />
+              </Badge>
+            </StyledLink>
+            <MenuIcon
+              onClick={() => setOpenSideMenu(true)}
+              sx={{ display: { md: "none" }, fontSize: "28px" }}
+            />
+          </Stack>
+        </Stack>
+      </Container>
+    </>
+  );
+};
+
+export default NavbarMiddle;
