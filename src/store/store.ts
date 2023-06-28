44c1d58f387d9027918d67edf69c326fd8376b15
sasanaryan <sasan4292@gmail.com>
2023-06-28 04:31:34 +0330

    feat: Create store for redux toolkit with redux-persist

---
+++ src/store/store.ts
@@ -0,0 +1,42 @@
+import { configureStore, combineReducers } from "@reduxjs/toolkit";
+import userReducer from "./userRedux";
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from "redux-persist";
+import storage from "redux-persist/lib/storage";
+import { TypedUseSelectorHook, useSelector, useDispatch } from "react-redux";
+
+const persistConfig = {
+  key: "root",
+  version: 1,
+  storage,
+};
+
+const rootReducer = combineReducers({ user: userReducer });
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+export const store = configureStore({
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
+});
+
+export let persistor = persistStore(store);
+
+// export state type
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
+export const useAppDispatch: () => AppDispatch = useDispatch;
+export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
