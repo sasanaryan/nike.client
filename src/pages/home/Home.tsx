5178bfd9da215385bbbe80f3684def1e8a603c95
sasanaryan <sasan4292@gmail.com>
2023-06-28 08:20:33 +0330

    feat: add categories component to home page

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,11 +1,13 @@
 import type { FC } from "react";
-import Footer from "../../components/footer/Footer";
-import Navbar from "../../components/navbar";
+import Footer from "components/footer";
+import Navbar from "components/navbar";
+import Categories from "components/categories";
 
 const Home: FC = () => {
   return (
     <>
       <Navbar />
+      <Categories />
       <Footer />
     </>
   );
