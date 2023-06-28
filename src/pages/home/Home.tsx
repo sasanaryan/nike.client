2dbf6b82bb5960cc5b3bf2855baf1b21a1c4c49f
sasanaryan <sasan4292@gmail.com>
2023-06-28 03:51:53 +0330

    feat: Create home page and integrate with route file

---
+++ src/pages/home/Home.tsx
@@ -0,0 +1,14 @@
+import type { FC } from "react";
+import Footer from "../../components/footer/Footer";
+import Navbar from "../../components/navbar";
+
+const Home: FC = () => {
+  return (
+    <>
+      <Navbar />
+      <Footer />
+    </>
+  );
+};
+
+export default Home;
--- src/route/RoutePage.tsx
+++ src/route/RoutePage.tsx
@@ -1,12 +1,14 @@
 import type { FC } from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import Register from "../pages/register";
-import Login from "../pages/login";
+import Register from "pages/register";
+import Login from "pages/login";
+import Home from "pages/home";
 
 const RoutePage: FC = () => {
   return (
     <Router>
       <Routes>
+        <Route path="/" element={<Home />} />
         <Route path="/register" element={<Register />} />
         <Route path="/login" element={<Login />} />
       </Routes>
