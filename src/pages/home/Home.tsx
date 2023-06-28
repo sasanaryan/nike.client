5b6c5c3467e42e5b58258fb88ffe15861f66e93a
sasanaryan <sasan4292@gmail.com>
2023-06-29 01:45:22 +0330

    feat: add products component to home page

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -2,12 +2,14 @@ import type { FC } from "react";
 import Footer from "components/footer";
 import Navbar from "components/navbar";
 import Categories from "components/categories";
+import Products from "components/products";
 
 const Home: FC = () => {
   return (
     <>
       <Navbar />
       <Categories />
+      <Products />
       <Footer />
     </>
   );
