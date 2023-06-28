39695c5558e06a55fc37761c355b6292a808d1b8
sasanaryan <sasan4292@gmail.com>
2023-06-28 08:37:57 +0330

    feat: add mui theme

--- src/theme/index.js
+++ src/theme/index.js
@@ -1,7 +1,7 @@
 import { css } from "styled-components";
-import { createTheme } from '@mui/material/styles'
+import { createTheme } from "@mui/material/styles";
 
-  export const mobile = (props) => {
+export const mobile = (props) => {
   return css`
     @media only screen and (max-width: 426px) {
       ${props}
@@ -16,3 +16,44 @@ export const tablet = (props) => {
     }
   `;
 };
+
+const theme = createTheme({
+  typography: {
+    fontFamily: [
+      "Manrope",
+      "-apple-system",
+      "BlinkMacSystemFont",
+      '"Segoe UI"',
+      "Roboto",
+      '"Helvetica Neue"',
+      "Arial",
+      "sans-serif",
+      '"Apple Color Emoji"',
+      '"Segoe UI Emoji"',
+      '"Segoe UI Symbol"',
+    ].join(","),
+  },
+  palette: {
+    primary: {
+      main: "#111111",
+      light: "#ffffff",
+      dark: "#111111",
+    },
+    secondary: {
+      main: "#e5e5e5",
+      dark: "#cccccc",
+      darker: "#0d0d0d",
+    },
+    textColors: {
+      blackFade: "#111111",
+      whiteFade: "#ffffff",
+      gray: "#7a7a7d",
+    },
+    backgroundColors: {
+      dark: "#111111",
+      light: "#ffffff",
+      gray: "#7a7a7d",
+    },
+  },
+});
+export default theme;
