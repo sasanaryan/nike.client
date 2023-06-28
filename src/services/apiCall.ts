ea91df42465d6c69014a89934d6498f018aed697
sasanaryan <sasan4292@gmail.com>
2023-06-28 08:36:57 +0330

    feat: create api call for register and login page

--- src/services/apiCall.ts
+++ src/services/apiCall.ts
@@ -1,53 +1,41 @@
-import { Start , loginSuccess ,Failure,logout } from "../store/userRedux";
-import {baseurl} from "../config";
+import { Start, loginSuccess, Failure, logout } from "store/userRedux";
+import { baseurl } from "config";
 import { Dispatch } from "redux";
-import { AppDispatch , useAppDispatch} from '../store/store'
+import { AppDispatch } from "store/store";
+import { UserRedux } from "type";
 
-type user = { 
-   _id?:number
-    username: string
-    email?: string
-    password:string
-    accessToken?:string
-}
-
-
-export const login = async (dispatch:AppDispatch, user:user)=>{
-    dispatch(Start());
-    try{
-        const res = await baseurl.post("/auth/login",user);
-        dispatch(loginSuccess(res.data));
-    }catch(err){
-        dispatch(Failure());
-    }
+export const login = async (dispatch: AppDispatch, user: UserRedux) => {
+  dispatch(Start());
+  try {
+    const res = await baseurl.post("/auth/login", user);
+    dispatch(loginSuccess(res.data));
+  } catch (err) {
+    dispatch(Failure());
+  }
 };
-export const registerUser = async (dispatch:Dispatch, user:user)=>{
-    dispatch(Start());
-    try{
-        const res = await baseurl.post("/auth/register",user);
-        dispatch(loginSuccess(res.data));
-    }catch(err){
-        dispatch(Failure());
-    }
+export const registerUser = async (dispatch: Dispatch, user: UserRedux) => {
+  dispatch(Start());
+  try {
+    const res = await baseurl.post("/auth/register", user);
+    dispatch(loginSuccess(res.data));
+  } catch (err) {
+    dispatch(Failure());
+  }
 };
 
-export const Delete = async (dispatch:Dispatch, user:user)=>{
-    
-    try{
-        const res = await baseurl.delete(`/user/`+user._id, {
-            headers: {
-                "Authorization" : `Bearer ${user.accessToken}`
-            }
-          });
-          dispatch(logout());
-        
-    }catch(err){
-        dispatch(Failure());  
-    }
-    
-};
-
-
-export const LogOut =  (dispatch:Dispatch)=>{
+export const Delete = async (dispatch: Dispatch, user: UserRedux) => {
+  try {
+    const res = await baseurl.delete(`/user/` + user._id, {
+      headers: {
+        Authorization: `Bearer ${user.accessToken}`,
+      },
+    });
     dispatch(logout());
+  } catch (err) {
+    dispatch(Failure());
+  }
+};
+
+export const LogOut = (dispatch: Dispatch) => {
+  dispatch(logout());
 };
