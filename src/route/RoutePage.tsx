4d0584863df7c10067970446e5717ce7ea659902
sasanaryan <sasan4292@gmail.com>
2023-06-30 16:53:55 +0330

    feact: Set route when user login for login and register page

--- src/route/RoutePage.tsx
+++ src/route/RoutePage.tsx
@@ -4,15 +4,17 @@ import Register from "pages/register";
 import Login from "pages/login";
 import Home from "pages/home";
 import Product from "pages/product";
+import { useAppSelector } from "store/store";
 
 const RoutePage: FC = () => {
+  const user = useAppSelector((state) => state.user.currentUser);
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/product/:id" element={<Product />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={user ? <Home /> : <Register />} />
+        <Route path="/login" element={user ? <Home /> : <Login />} />
       </Routes>
     </Router>
   );
