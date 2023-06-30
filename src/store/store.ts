3aa74b221da6d2b43084f7e787e905494245f855
sasanaryan <sasan4292@gmail.com>
2023-06-30 13:56:03 +0330

    feat: insert cartRedux to store

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,5 +1,6 @@
 import { configureStore, combineReducers } from "@reduxjs/toolkit";
-import userReducer from "./userRedux";
+import userReducer from "store/userRedux";
+import cartReducer from "store/cartRedux";
 import {
   persistStore,
   persistReducer,
@@ -19,7 +20,7 @@ const persistConfig = {
   storage,
 };
 
-const rootReducer = combineReducers({ user: userReducer });
+const rootReducer = combineReducers({ user: userReducer, cart: cartReducer });
 
 const persistedReducer = persistReducer(persistConfig, rootReducer);
 
