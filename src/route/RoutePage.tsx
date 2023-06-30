50d411acb982c2dbb25a92432c4812ff3aa8a0db
sasanaryan <sasan4292@gmail.com>
2023-06-30 12:46:37 +0330

    feat: add product page to route

--- src/route/RoutePage.tsx
+++ src/route/RoutePage.tsx
@@ -3,12 +3,14 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Register from "pages/register";
 import Login from "pages/login";
 import Home from "pages/home";
+import Product from "pages/product";
 
 const RoutePage: FC = () => {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/product/:id" element={<Product />} />
         <Route path="/register" element={<Register />} />
         <Route path="/login" element={<Login />} />
       </Routes>
