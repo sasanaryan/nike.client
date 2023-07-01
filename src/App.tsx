13ce36bd2b3da19adf4b5a746141bae85740499a
sasanaryan <sasan4292@gmail.com>
2023-07-01 07:04:11 +0330

    feat: Add theme through ThemeProvider to app

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
 import type { FC } from "react";
+import { ThemeProvider } from "@mui/material/styles";
+import theme from "./theme";
 import Routes from "./route";
 
 const App: FC = () => {
   return (
     <>
-      <Routes />
+      <ThemeProvider theme={theme}>
+        <Routes />
+      </ThemeProvider>
     </>
   );
 };
