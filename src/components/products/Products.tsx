e52deba921c7e90fb2400a1aae1241a8d7b99ee6
sasanaryan <sasan4292@gmail.com>
2023-07-01 06:58:13 +0330

    feact: Add api call and filter functionality to products component
    -add api call for fetch product base tow method (with category and without category)
    -add filter functionality for sort and price range and price sort

--- src/components/products/Products.tsx
+++ src/components/products/Products.tsx
@@ -1,15 +1,66 @@
+import { useState, useEffect } from "react";
 import type { FC } from "react";
+import { FilterProducts } from "type";
 import { FetchedProduct } from "type";
+import Grid from "@mui/material/Unstable_Grid2";
+import { baseurl } from "config";
 import Product from "components/product";
-import { Mockproducts } from "data";
 
-const Products: FC = () => {
+const Products: FC<FilterProducts> = ({ gender, range, cat, sort }) => {
+  const [products, setProducts] = useState<FetchedProduct[]>([]);
+  const [filter, setFilter] = useState<FetchedProduct[]>([]);
+
+  useEffect(() => {
+    const getProducts = async () => {
+      try {
+        const res = await baseurl.get(
+          cat ? `/products?category=${cat}` : "/products"
+        );
+        setProducts(res.data);
+        setFilter(res.data);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    getProducts();
+  }, [cat]);
+
+  useEffect(() => {
+    range &&
+      setFilter(
+        products.filter(
+          (item) => item.price <= range[1] && range[0] <= item.price
+        )
+      );
+
+    gender !== "No gender" &&
+      setFilter((prev) => [...prev].filter((item) => item.gender === gender));
+  }, [range, gender]);
+
+  useEffect(() => {
+    if (sort === "newest") {
+      setFilter((prev) =>
+        [...prev].sort((a, b) => +a.createdAt! - +b.createdAt!)
+      );
+    } else if (sort === "asc") {
+      setFilter((prev) => [...prev].sort((a, b) => a.price - b.price));
+    } else {
+      setFilter((prev) => [...prev].sort((a, b) => b.price - a.price));
+    }
+  }, [sort]);
+
   return (
-    <>
-      {Mockproducts.map((item: FetchedProduct) => (
-        <Product item={item} key={item._id} />
-      ))}
-    </>
+    <Grid container margin={{ xs: "0px", sm: "5px" }}>
+      {cat
+        ? filter.map((item: FetchedProduct) => (
+            <Product item={item} key={item._id} />
+          ))
+        : products
+            .slice(0, 12)
+            .map((item: FetchedProduct) => (
+              <Product item={item} key={item._id} />
+            ))}
+    </Grid>
   );
 };
 
