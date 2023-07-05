58a21915721559625f83c8ba31e3c93f56961ab1
sasanaryan <sasan4292@gmail.com>
2023-07-05 22:49:14 +0330

    feat: Create favorite component for show favorite product in profile page

---
+++ src/components/favorit/Favourites.tsx
@@ -0,0 +1,47 @@
+import { useEffect, useState } from "react";
+import type { FC } from "react";
+import { FetchedProduct } from "type";
+import { baseurl } from "config";
+import { useAppSelector } from "store/store";
+import Product from "components/product/Product";
+import { Grid } from "@mui/material";
+
+const Favourits: FC = () => {
+  const [favoriteProducts, setFavoriteProducts] = useState<
+    FetchedProduct[] | null
+  >();
+  const [products, setProducts] = useState<FetchedProduct[]>([]);
+  const [loading, setLoading] = useState(true);
+  const user = useAppSelector((state) => state.user.currentUser);
+
+  useEffect(() => {
+    const getProducts = async () => {
+      try {
+        const res = await baseurl.get("/products");
+        setProducts(res.data);
+        setLoading(false);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    getProducts();
+  }, []);
+
+  useEffect(() => {
+    setFavoriteProducts(
+      products.filter((product: FetchedProduct) =>
+        user?.favorites?.includes(product._id)
+      )
+    );
+  }, [products]);
+
+  return (
+    <Grid container padding={{ xs: "0px", sm: "5px" }}>
+      {favoriteProducts?.map((product: FetchedProduct) => (
+        <Product key={product._id} item={product} />
+      ))}
+    </Grid>
+  );
+};
+
+export default Favourits;
