cef8a8cad8d4920a7ca6343b2af0b92e80a31ff2
sasanaryan <sasan4292@gmail.com>
2023-07-06 16:14:19 +0330

    feat: provide status context and insert alert component in app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,17 @@ import type { FC } from "react";
 import { ThemeProvider } from "@mui/material/styles";
 import theme from "./theme";
 import Routes from "./route";
+import StatusProvider from "context/StatusProvider";
+import Snackbars from "components/alert";
 
 const App: FC = () => {
   return (
     <>
       <ThemeProvider theme={theme}>
-        <Routes />
+        <StatusProvider>
+          <Routes />
+          <Snackbars />
+        </StatusProvider>
       </ThemeProvider>
     </>
   );
