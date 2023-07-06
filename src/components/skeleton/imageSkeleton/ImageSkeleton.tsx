3d80b65889de7e758ae1a9583fe1a8295e434b9b
sasanaryan <sasan4292@gmail.com>
2023-07-07 01:13:15 +0330

    feat: create imageSkeleton component for Display a placeholder preview of product images

---
+++ src/components/skeleton/imageSkeleton/ImageSkeleton.tsx
@@ -0,0 +1,43 @@
+import type { FC } from "react";
+import { Skeleton } from "@mui/material";
+import { Stack } from "@mui/system";
+
+const ImageSkeleton: FC = () => {
+  return (
+    <Stack
+      direction={{ xs: "column", md: "row", lg: "row" }}
+      gap={2}
+      flex="2.5"
+      height="100%"
+      margin={{ xl: "10px", sm: "0" }}
+    >
+      <Stack
+        direction={{ xs: "row", md: "column", lg: "column" }}
+        display={{ xs: "none", sm: "flex" }}
+      >
+        {Array(8)
+          .fill(1)
+          .map(() => (
+            <Skeleton
+              variant="rectangular"
+              sx={{
+                width: "60px",
+                height: "60px",
+                margin: "4px",
+                borderRadius: "5px",
+              }}
+            />
+          ))}
+      </Stack>
+      <Skeleton
+        variant="rectangular"
+        sx={{
+          width: "100%",
+          height: { xs: "360px", sm: "871px", md: "748px", lg: "837px" },
+        }}
+      />
+    </Stack>
+  );
+};
+
+export default ImageSkeleton;
