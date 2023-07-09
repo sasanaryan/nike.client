c2a60172819cae554351a41e1a571cf98b4343e7
sasanaryan <sasan4292@gmail.com>
2023-07-09 04:18:30 +0330

    feat: Create interceptors middlware in the custom hook form for geting new AccessToken by refreshToken

---
+++ src/feature/useTokenRequst.ts
@@ -0,0 +1,49 @@
+import axios from "axios";
+import jwt_decode from "jwt-decode";
+import dayjs from "dayjs";
+import { useAppDispatch, useAppSelector } from "store/store";
+import { logout, RefreshToken } from "store/userRedux";
+import { useNavigate } from "react-router-dom";
+
+interface MyToken {
+  name: string;
+  exp: number;
+}
+const localURL = "http://localhost:8800/api/";
+
+const useTokenRequst = () => {
+  const navigate = useNavigate();
+  const dispatch = useAppDispatch();
+  const user = useAppSelector((state) => state.user);
+
+  const axiosInstance = axios.create({
+    baseURL: localURL,
+    headers: { Authorization: `Bearer ${user?.accessToken}` },
+  });
+
+  axiosInstance.interceptors.request.use(async (req) => {
+    req.headers.Authorization = `Bearer ${user?.accessToken!}`;
+    const userToken: MyToken = jwt_decode(user?.accessToken!);
+    const isExpired = dayjs.unix(userToken.exp).diff(dayjs()) < 1;
+
+    if (!isExpired) return req;
+    try {
+      const response = await axios.post(`${localURL}refreshtoken`, {
+        refreshToken: user?.currentUser?.refreshToken,
+      });
+      const newAccessToken = response.data.accessToken;
+      dispatch(RefreshToken(newAccessToken));
+      req.headers.Authorization = `Bearer ${newAccessToken}`;
+      return req;
+    } catch (err) {
+      navigate("/login");
+      dispatch(logout());
+      req.headers.Authorization = null;
+      return req;
+    }
+  });
+
+  return axiosInstance;
+};
+
+export default useTokenRequst;
