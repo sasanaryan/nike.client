2762c16be1793ff86dba9c1bc75ea783cbfebc31
sasanaryan <sasan4292@gmail.com>
2023-07-13 06:27:20 +0330

    perf: Change importing to dynamic with lazy loading
    with dynamic and lazy loading  bundle size decrease to 2.8 MB and initial loading is best optimaize

--- src/route/RoutePage.tsx
+++ src/route/RoutePage.tsx
@@ -1,27 +1,31 @@
+import React from "react";
 import type { FC } from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import Register from "pages/register";
-import Login from "pages/login";
-import Home from "pages/home";
-import Product from "pages/product";
 import { useAppSelector } from "store/store";
-import ProductList from "pages/productList/ProductList";
-import Cart from "pages/cart";
-import Profile from "pages/profile";
+import Loading from "components/loading";
+const Register = React.lazy(() => import("pages/register"));
+const Login = React.lazy(() => import("pages/login"));
+const Home = React.lazy(() => import("pages/home"));
+const ProductList = React.lazy(() => import("pages/productList"));
+const Cart = React.lazy(() => import("pages/cart"));
+const Profile = React.lazy(() => import("pages/profile"));
+const Product = React.lazy(() => import("pages/product"));
 
 const RoutePage: FC = () => {
   const user = useAppSelector((state) => state.user.currentUser);
   return (
     <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/products/:category?" element={<ProductList />} />
-        <Route path="/product/:id" element={<Product />} />
-        <Route path="/cart" element={<Cart />} />
-        <Route path="/register" element={user ? <Home /> : <Register />} />
-        <Route path="/login" element={user ? <Home /> : <Login />} />
-        <Route path="/profile/:sectionParam?" element={<Profile />} />
-      </Routes>
+      <React.Suspense fallback={<Loading />}>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/products/:category?" element={<ProductList />} />
+          <Route path="/product/:id" element={<Product />} />
+          <Route path="/cart" element={<Cart />} />
+          <Route path="/profile/:sectionParam?" element={<Profile />} />
+          <Route path="/register" element={user ? <Home /> : <Register />} />
+          <Route path="/login" element={user ? <Home /> : <Login />} />
+        </Routes>
+      </React.Suspense>
     </Router>
   );
 };
