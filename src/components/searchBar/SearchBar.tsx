626b7083d878acde29ad7795fd217e86f2d3bc9a
sasanaryan <sasan4292@gmail.com>
2023-07-13 05:07:46 +0330

    perf: Memoized SearchBar Component

--- src/components/searchBar/SearchBar.tsx
+++ src/components/searchBar/SearchBar.tsx
@@ -1,4 +1,4 @@
-import React, { useRef, useState } from "react";
+import React, { useRef, useState, memo } from "react";
 import type { FC } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { Typography } from "@mui/material";
@@ -115,4 +115,4 @@ const SearchBar: FC<SearchBarProp> = ({ setOpenSearchBar, openSearchBar }) => {
   );
 };
 
-export default SearchBar;
+export default memo(SearchBar);
